fdeacde99cf6a57f122a142add64e44a98f1ec20
Rounakdadsena <87689078+RounakDadsena@users.noreply.github.com>
2025-03-22 19:05:47 +0530

    Update vidsrcsu.ts

--- src/providers/sources/vidsrcsu.ts
+++ src/providers/sources/vidsrcsu.ts
@@ -108,7 +108,7 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
 export const vidsrcsuScraper = makeSourcerer({
   id: 'vidsrcsu',
   name: 'filmu 3',
-  rank: 170,
+  rank: 270,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
