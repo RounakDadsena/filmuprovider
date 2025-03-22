ef8bc979cd85e4c9c562ff4e7f60a67504fd1c09
Rounakdadsena <87689078+RounakDadsena@users.noreply.github.com>
2025-03-22 19:24:22 +0530

    Update streambox.ts

--- src/providers/sources/streambox.ts
+++ src/providers/sources/streambox.ts
@@ -98,7 +98,7 @@ export const streamboxScraper = makeSourcerer({
   id: 'streambox',
   name: 'filmu 2',
   rank: 370,
-  disabled: false,
+  disabled: true,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
